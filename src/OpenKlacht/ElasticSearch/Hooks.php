48a1eb686502bd813448524b371af6cc3df9ac58
Mike van den Hoek <mike@yard.nl>
2024-12-10 08:38:38 +0100

    (fix): incorrect use of nullable operator

--- src/OpenKlacht/ElasticSearch/Hooks.php
+++ src/OpenKlacht/ElasticSearch/Hooks.php
@@ -10,11 +10,11 @@ class Hooks
             return $args;
         }
 
-        $args['okl_judgement_date'] = get_post_meta($postID, 'okl_judgement_date', true) ?? '';
-        $args['okl_judgement_year'] = get_post_meta($postID, 'okl_judgement_year', true) ?? '';
-        $args['okl_date_received'] = get_post_meta($postID, 'okl_date_received', true) ?? '';
-        $args['okl_year_received'] = get_post_meta($postID, 'okl_year_received', true) ?? '';
-        $args['okl_judgement'] = get_post_meta($postID, 'okl_judgement', true) ?? '';
+        $args['okl_judgement_date'] = get_post_meta($postID, 'okl_judgement_date', true) ?: '';
+        $args['okl_judgement_year'] = get_post_meta($postID, 'okl_judgement_year', true) ?: '';
+        $args['okl_date_received'] = get_post_meta($postID, 'okl_date_received', true) ?: '';
+        $args['okl_year_received'] = get_post_meta($postID, 'okl_year_received', true) ?: '';
+        $args['okl_judgement'] = get_post_meta($postID, 'okl_judgement', true) ?: '';
 
         return $args;
     }
