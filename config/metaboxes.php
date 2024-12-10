14a164354edcedb7864b10aa0186d9c6ed837bca
Mike van den Hoek <mike@yard.nl>
2024-12-10 08:34:16 +0100

    (refactor): metaboxes of type textarea to wysiwyg

--- config/metaboxes.php
+++ config/metaboxes.php
@@ -6,16 +6,16 @@ return [
     ['name' => 'Ontvangstdatum', 'id' => 'date_received', 'type' => 'text'],
     ['name' => 'Betrokken organisatie onderdeel', 'id' => 'organization', 'type' => 'text'],
     ['name' => 'Functiebenaming ambtenaar over wie geklaagd is', 'id' => 'function', 'type' => 'text'],
-    ['name' => 'Omschrijving', 'id' => 'description', 'type' => 'textarea'],
-	['name' => 'Bevinding', 'id' => 'findings', 'type' => 'textarea'],
-	['name' => 'Oordeel', 'id' => 'judgement', 'type' => 'select', 'options' =>
+    ['name' => 'Omschrijving', 'id' => 'description', 'type' => 'wysiwyg'],
+    ['name' => 'Bevinding', 'id' => 'findings', 'type' => 'wysiwyg'],
+    ['name' => 'Oordeel', 'id' => 'judgement', 'type' => 'select', 'options' =>
         [
-			'Gegrond' => 'Gegrond',
-			'Deels gegrond, deels ongegrond' => 'Deels gegrond, deels ongegrond',
-			'Ongegrond' => 'Ongegrond',
-			'Niet ontvankelijk' => 'Niet ontvankelijk',
-		],
+            'Gegrond' => 'Gegrond',
+            'Deels gegrond, deels ongegrond' => 'Deels gegrond, deels ongegrond',
+            'Ongegrond' => 'Ongegrond',
+            'Niet ontvankelijk' => 'Niet ontvankelijk',
+        ],
     ],
-    ['name' => 'Conclusie', 'id' => 'conclusion', 'type' => 'textarea'],
-    ['name' => 'Dagtekening van het oordeel', 'id' => 'judgement_date', 'type' => 'text']
+    ['name' => 'Conclusie', 'id' => 'conclusion', 'type' => 'wysiwyg'],
+    ['name' => 'Dagtekening van het oordeel', 'id' => 'judgement_date', 'type' => 'text'],
 ];
